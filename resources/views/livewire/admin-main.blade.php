cd726a93f32d0f92d4843db2c63664d080e0d4f4
Jorge Valdes Narvaez <jorge.vnarvaez@gmail.com>
2021-06-06 18:18:00 -0400

    bug

--- resources/views/livewire/admin-main.blade.php
+++ resources/views/livewire/admin-main.blade.php
@@ -1,12 +1,12 @@
 <div>
   <!-- This example requires Tailwind CSS v2.0+ -->
-<div class="mt-10 w-full">
-  <div class="">
-    <div class="py-2 min-w-full sm:px-6 lg:px-8">
-      <div class="shadow border-b border-gray-200 sm:rounded-lg">
-        <table class="min-w-full divide-y overflow-hidden divide-gray-200">
+<div class="mt-10 inline-block w-full">
+  <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+      <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+        <table class="min-w-full divide-y divide-gray-200">
           <thead class="bg-gray-50">
-            <tr class="flex flex-col flex-no wrap sm:table-row">
+            <tr>
               <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                 Name
               </th>
@@ -24,8 +24,8 @@
               </th>
             </tr>
           </thead>
-          <tbody class="bg-white flex-1 sm:flex-none divide-y divide-gray-200">
-            <tr class="flex flex-col flex-no wrap sm:table-row">
+          <tbody class="bg-white divide-y divide-gray-200">
+            <tr>
               <td class="px-6 py-4 whitespace-nowrap">
                 <div class="flex items-center">
                   <div class="flex-shrink-0 h-10 w-10">
@@ -61,7 +61,6 @@
             <!-- More people... -->
           </tbody>
         </table>
-
       </div>
     </div>
   </div>
